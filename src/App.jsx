c17e41019f9c1dc1d370f63e62839f9da8507d16
Julián Millar <julianjmillar@gmail.com>
2024-08-27 08:41:44 -0400

    advance

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,53 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [votos, setVotos] = useState(0)
+
+  const KuoLi = 'Kuo-li'
+  const CumsilleDiez = 'Cumsille-Diez'
+  const MillarMedel = 'Millar-Medel'
+  const MendezMolina = 'Mendez-Molina'
+
+  let votaciones = {
+    kuoli: 0,
+    mendezmolina: 0,
+    cumsillediez: 0,
+    millarmedel: 0
+  }
+
+  function vote() {
+    votaciones.kuoli += parseInt(document.getElementById(KuoLi).value)
+    votaciones.cumsillediez += parseInt(document.getElementById(CumsilleDiez).value)
+    votaciones.millarmedel += parseInt(document.getElementById(MillarMedel).value)
+    votaciones.mendezmolina += parseInt(document.getElementById(MendezMolina).value)    
+    setVotos(votos + 1)
+    console.log(votaciones)
+    console.log(votos);
+    event.preventDefault()
+  }
 
   return (
-    <main>
-      hola choco
-    </main>
+    <form id='vote__form'>
+      <div className='wrapper'>
+        <label>{`Familia ${KuoLi}`}</label>
+        <input type="number" name={KuoLi} id={KuoLi} />
+      </div>
+      <div className='wrapper'>
+        <label>{`Familia ${MendezMolina}`}</label>
+        <input type="number" name={MendezMolina} id={MendezMolina} />
+      </div>
+      <div className='wrapper'>
+        <label>{`Familia ${CumsilleDiez}`}</label>
+        <input type="number" name={CumsilleDiez} id={CumsilleDiez} />
+      </div>
+      <div className='wrapper'>
+        <label>{`Familia ${MillarMedel}`}</label>
+        <input type="number" name={MillarMedel} id={MillarMedel} />
+      </div>
+      <button onClick={vote}>VOTAR!</button>
+      <button>Reset</button>
+      <p>{`Votos escrutados: ${votos}`}</p>
+    </form>
   )
 }
 
